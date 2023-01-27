8454e644b4ec6f38a706d892fb1473ab4b24df02
keldaan <arnaudgregoire92@gmail.com>
2023-01-27 18:51:51 +0100

    impl decay

---
+++ index.ts
@@ -0,0 +1,47 @@
+import { connect } from "mongoose"
+import dotenv from "dotenv"
+import userMetadata from "./models/user-metadata"
+import detailledStatistic from "./models/detailled-statistic"
+
+// This is your task's entrypoint. When your task is executed, this
+// function will be called.
+export default async function main() {
+  dotenv.config()
+  const db = await connect(process.env.MONGO_URI!)
+  const users = await userMetadata.find(
+    { elo: { $gt: 900 } },
+    ["uid", "elo", "displayName"],
+    { sort: { level: -1 } }
+  )
+  if (users) {
+    for (let i = 0; i < users.length; i++) {
+      const u = users[i]
+      const stats = await detailledStatistic.find(
+        { playerId: u.uid },
+        ["time"],
+        {
+          limit: 1,
+          sort: { time: -1 }
+        }
+      )
+      if (stats && stats.length > 0) {
+        const time = stats[0].time
+        if (time) {
+          const lastGame = new Date(time)
+          const now = new Date(Date.now())
+          if (now.getTime() - lastGame.getTime() > 86400 * 1000 * 10) {
+            const decay = Math.max(1200, u.elo - 10)
+            console.log(
+              `user ${u.displayName} (${u.elo}) will decay to ${decay}`
+            )
+            u.elo = decay
+            u.save()
+          }
+        }
+      }
+    }
+  }
+  await db.disconnect()
+}
+
+main()
---
+++ models/detailled-statistic.ts
@@ -0,0 +1,61 @@
+import { Schema, model } from "mongoose"
+import { Pkm, Item } from "../types"
+
+export interface Pokemon {
+  name: string
+  avatar: string
+  items: string[]
+}
+
+export interface IDetailledStatistic {
+  playerId: string
+  elo: number
+  time: number
+  name: string
+  rank: number
+  avatar: string
+  pokemons: Pokemon[]
+}
+
+const pokemon = new Schema({
+  name: {
+    type: String,
+    enum: Object.values(Pkm)
+  },
+  avatar: {
+    type: String
+  },
+  items: [
+    {
+      type: String,
+      enum: Item
+    }
+  ]
+})
+
+const statisticSchema = new Schema({
+  playerId: {
+    type: String
+  },
+  elo: {
+    type: Number
+  },
+  time: {
+    type: Number
+  },
+  name: {
+    type: String
+  },
+  rank: {
+    type: Number
+  },
+  avatar: {
+    type: String
+  },
+  pokemons: [pokemon]
+})
+
+export default model<IDetailledStatistic>(
+  "DetailledStatisticV2",
+  statisticSchema
+)
