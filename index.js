3efc1b689ad070c0dd8f2aac148e172d17cd62fd
Keldaan <keldaan.ag@gmail.com>
2024-05-14 10:20:03 +0200

    Revert "disable elo decay temporarly"
    This reverts commit bdd86baced3bf28bcd36b0ff8e747c575c34503c.

--- index.js
+++ index.js
@@ -352,6 +352,42 @@ async function main() {
   import_dotenv.default.config();
   const db = await (0, import_mongoose5.connect)(process.env.MONGO_URI);
   try {
+    const users = await user_metadata_default.find(
+      { elo: { $gt: 1100 } },
+      ["uid", "elo", "displayName"],
+      { sort: { level: -1 } }
+    );
+    if (users && users.length > 0) {
+      console.log("Checking activity of ", users.length, " users");
+      for (let i = 0; i < users.length; i++) {
+        const u = users[i];
+        const stats = await detailled_statistic_default.find(
+          { playerId: u.uid },
+          ["time"],
+          {
+            limit: 1,
+            sort: { time: -1 }
+          }
+        );
+        if (stats && stats.length > 0) {
+          const time = stats[0].time;
+          if (time) {
+            const lastGame = new Date(time);
+            const now = new Date(Date.now());
+            if (now.getTime() - lastGame.getTime() > 86400 * 1e3 * 10) {
+              const decay = Math.max(1100, u.elo - 10);
+              console.log(
+                `user ${u.displayName} (${u.elo}) will decay to ${decay}`
+              );
+              u.elo = decay;
+              await u.save();
+            }
+          }
+        }
+      }
+    } else {
+      console.log("No users to check");
+    }
     console.log("count the numbers of users...");
     const count = await user_metadata_default.countDocuments();
     console.log(count, " users found");
--- index.ts
+++ index.ts
@@ -12,42 +12,42 @@ export default async function main() {
   dotenv.config()
   const db = await connect(process.env.MONGO_URI!)
   try {
-    // const users = await userMetadata.find(
-    //   { elo: { $gt: 1100 } },
-    //   ["uid", "elo", "displayName"],
-    //   { sort: { level: -1 } }
-    // )
-    // if (users && users.length > 0) {
-    //   console.log("Checking activity of ", users.length, " users")
-    //   for (let i = 0; i < users.length; i++) {
-    //     const u = users[i]
-    //     const stats = await detailledStatistic.find(
-    //       { playerId: u.uid },
-    //       ["time"],
-    //       {
-    //         limit: 1,
-    //         sort: { time: -1 }
-    //       }
-    //     )
-    //     if (stats && stats.length > 0) {
-    //       const time = stats[0].time
-    //       if (time) {
-    //         const lastGame = new Date(time)
-    //         const now = new Date(Date.now())
-    //         if (now.getTime() - lastGame.getTime() > 86400 * 1000 * 10) {
-    //           const decay = Math.max(1100, u.elo - 10)
-    //           console.log(
-    //             `user ${u.displayName} (${u.elo}) will decay to ${decay}`
-    //           )
-    //           u.elo = decay
-    //           await u.save()
-    //         }
-    //       }
-    //     }
-    //   }
-    // } else {
-    //   console.log("No users to check")
-    // }
+    const users = await userMetadata.find(
+      { elo: { $gt: 1100 } },
+      ["uid", "elo", "displayName"],
+      { sort: { level: -1 } }
+    )
+    if (users && users.length > 0) {
+      console.log("Checking activity of ", users.length, " users")
+      for (let i = 0; i < users.length; i++) {
+        const u = users[i]
+        const stats = await detailledStatistic.find(
+          { playerId: u.uid },
+          ["time"],
+          {
+            limit: 1,
+            sort: { time: -1 }
+          }
+        )
+        if (stats && stats.length > 0) {
+          const time = stats[0].time
+          if (time) {
+            const lastGame = new Date(time)
+            const now = new Date(Date.now())
+            if (now.getTime() - lastGame.getTime() > 86400 * 1000 * 10) {
+              const decay = Math.max(1100, u.elo - 10)
+              console.log(
+                `user ${u.displayName} (${u.elo}) will decay to ${decay}`
+              )
+              u.elo = decay
+              await u.save()
+            }
+          }
+        }
+      }
+    } else {
+      console.log("No users to check")
+    }
 
     console.log("count the numbers of users...")
     const count = await userMetadata.countDocuments()
